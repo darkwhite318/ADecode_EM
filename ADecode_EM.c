d385a8107eaf28b5bdcb88c917f15d3f66f83c9a
darkwhite318 <r05944002@ntu.edu.tw>
2016-10-14 17:37:28 +0800

    change SSIZE to non-fix number and add a function calcSSIZE to calculate it. fix the bug of parsing input text (the last character will fail for no SPACE  at the end or somehow)

--- ADecode_EM.c
+++ ADecode_EM.c
@@ -13,11 +13,14 @@
 #include <vector>
 
 using namespace std;
-# define SSIZE 37
+//# define SSIZE 37
 
-void initEncode(char*,int(&EncodeCk)[SSIZE][SSIZE],vector<vector<double> >(&Encode),vector<vector<int> >(&idx));
-void initBigram(double(&bigram)[SSIZE][SSIZE],vector<int>(&textInt),vector<vector<int> >(&idx));
+void initEncode(char*,int**EncodeCk,vector<vector<double> >(&Encode),vector<vector<int> >(&idx));
+void initBigram(double**bigram,vector<int>(&textInt),vector<vector<int> >(&idx));
 int c2n(string);
+int calcSSIZE(char*);
+
+int SSIZE=0;
 
 // ./ADecode_EM encode.bin test.num out.txt
 int main(int argc, char* argv[])
@@ -35,31 +38,51 @@ int main(int argc, char* argv[])
    vector<int> textInt;
    char split_char = ' ';
    istringstream split(text);
-   for(string each;getline(split,each,split_char);textInt.push_back(c2n(each)));
-   
+   int num;
+   for(string each;getline(split,each,split_char);)
+      {
+	   num = c2n(each);
+	   if(num>0)
+	     textInt.push_back(num);
+      }
+
    textFile.close();
 
+   //calculate SSIZE
+   SSIZE = calcSSIZE(argv[1]);
+   
    //initialize Encode table
-   int EncodeCk[SSIZE][SSIZE];//used for quick check//0: prob=0, num:num's element in the vector
+   int ** EncodeCk;//used for quick check//0: prob=0, num:num's element in the vector
+   EncodeCk = new int*[SSIZE];
+   for(int i=0;i<SSIZE;i++)
+      EncodeCk[i] = new int[SSIZE];
    vector<vector<int> > EncodeIdx;
    vector<vector<double> > Encode;//store the encode prob
    initEncode(argv[1],EncodeCk,Encode,EncodeIdx);
+   //cout<<"Encode end"<<endl;
+ 
+
 
    //initialize Bigram table
-   double bigram[SSIZE][SSIZE];
+   double ** bigram;//used for quick check//0: prob=0, num:num's element in the vector
+   bigram = new double*[SSIZE];
+   for(int i=0;i<SSIZE;i++)
+      bigram[i] = new double[SSIZE];
    initBigram(bigram,textInt,EncodeIdx);
+   
+  /* 
    for(int i=0;i<SSIZE;i++){
       for(int j=0;j<SSIZE;j++){
-        cout<<bigram[i][j]<<" ";
+        cout<<EncodeCk[i][j]<<" ";
       }
       cout<<endl;
    }
-
+   */
 
 /*
-   vector<vector<double> >::iterator it1;
-   vector<double>::iterator  it2;
-   for(it1 = Encode.begin();it1!=Encode.end();it1++){
+   vector<vector<int> >::iterator it1;
+   vector<int>::iterator  it2;
+   for(it1 = EncodeIdx.begin();it1!=EncodeIdx.end();it1++){
       for(it2 = it1->begin();it2!=it1->end();it2++){
          cout<<*it2<<" ";
       }
@@ -76,47 +99,43 @@ int main(int argc, char* argv[])
 return 0;
 }
 
-void initEncode(char* fileName, int(&EncodeCk)[SSIZE][SSIZE],vector<vector<double> >(&Encode),vector<vector<int> >(&idx))
+void initEncode(char* fileName, int**EncodeCk,vector<vector<double> >(&Encode),vector<vector<int> >(&idx))
 {
-//NOTE: we assume that the order of the encode table will obey the order
-//0 0 x
-//0 1 x
-//...
-//0 36x
-//1 0 x
-//...
 
 //output:each row show the prob of all the true value encoded by k
 
    fstream file;//encode.num
    char buffer[20];//buffer for each line
-   double lineSum;//calculate number of n0n-zero element in each row
    double validNum[SSIZE];
-   memset(EncodeCk,0,sizeof(EncodeCk));
+   char split_char = ' ';
+   int parser[3];
+
+   //memset(EncodeCk,0,sizeof(EncodeCk));
    memset(validNum,0,sizeof(validNum));
    file.open(fileName,ios::in);
    if(!file)
       cerr<<"can not read encode file"<<endl;
    else
       {
-      for(int i=0;i<SSIZE;i++)
-         {
-	 for(int j=0;j<SSIZE;j++)
-	    {
-            file.getline(buffer,sizeof(buffer),'\n');
-            EncodeCk[j][i] = buffer[strlen(buffer)-1]-'0'; // i(true value) is encoded by j(observation)
-	    if(EncodeCk[j][i])
-	      validNum[i]++;
-	    }
-	 }
+           while( file.getline(buffer,sizeof(buffer),'\n'))
+	        {
+		int counter =0;
+                istringstream str(buffer);
+		for(string each;getline(str,each,split_char);counter++)
+		   {
+		      parser[counter] = c2n(each);
+		      //cout<<counter<<":"<<parser[counter]<<" ";
+		   }
+		EncodeCk[parser[1]][parser[0]] = parser[2]; // i(true value) is encoded by j(observation)
+	        
+		if(EncodeCk[parser[1]][parser[0]])
+	          validNum[parser[0]]++;
+		}
 
       for(int i=0;i<SSIZE;i++)
          {
- 	 lineSum=0;
    	 vector<double> EncodeLine;
 	 vector<int> idxLine;
-	 for(int j=0;j<SSIZE;j++)
-	    lineSum = lineSum+EncodeCk[i][j];
 
          double lineiter=0;
 	 for(int j=0;j<SSIZE;j++)	   
@@ -126,40 +145,51 @@ void initEncode(char* fileName, int(&EncodeCk)[SSIZE][SSIZE],vector<vector<doubl
 	      lineiter++;
 	      EncodeLine.push_back(double(1/validNum[j]));
 	      idxLine.push_back(j);
-	      //cout<<j<<" ";
   	      EncodeCk[i][j] = lineiter;
 	      }
 	    }
 	 idx.push_back(idxLine);
 	 Encode.push_back(EncodeLine);
-         //cout<<endl;
 	 }
      }
 }
 
 int c2n(string bu)
 {
-   if(bu.length() >1)
+   if(bu.length() >1 && (bu[1]-'0')>0 && (bu[1]-'0')<10)
      return 10*(bu[0]-'0')+(bu[1]-'0');
    else
      return (bu[0]-'0');
 }
 
-void initBigram(double(&bigram)[SSIZE][SSIZE],vector<int> (&textInt),vector<vector<int> >(&idx))
+void initBigram(double**bigram,vector<int> (&textInt),vector<vector<int> >(&idx))
 {
-   memset(bigram,0,sizeof(bigram));
+   //memset(bigram,0,SSIZE*SSIZE*sizeof(*bigram));
+   for(int i=0;i<SSIZE;i++)
+      {
+      for(int j=0;j<SSIZE;j++)
+         {
+         bigram[i][j]=0;
+	 }
+      }
    vector<int>::iterator iterTI;
    vector<int>::iterator iterIdx1;
    vector<int>::iterator iterIdx2;
-   for(iterTI = textInt.begin();iterTI!=textInt.end()-1;iterTI++)
+   for(iterTI = textInt.begin()+1;iterTI!=textInt.end();iterTI++)
       {
-      for(iterIdx1 = idx[*iterTI].begin();iterIdx1!=idx[*iterTI].end();iterIdx1++)
+       //cout<<*iterTI<<endl;     
+      for(iterIdx1 = idx[*(iterTI-1)].begin();iterIdx1!=idx[*(iterTI-1)].end();iterIdx1++)
          {
-         for(iterIdx2 = idx[*(iterTI+1)].begin();iterIdx2!=idx[*(iterTI+1)].end();iterIdx2++)
+         for(iterIdx2 = idx[*(iterTI)].begin();iterIdx2!=idx[*(iterTI)].end();iterIdx2++)
 	    {
+	    //cout<<*iterIdx1<<" "<<*iterIdx2<<" "<< bigram[*iterIdx1][*iterIdx2]<<"|";
+	    //cout<<*iterIdx1<<" "<<*iterIdx2<<"|";
 	    bigram[*iterIdx1][*iterIdx2]++;
 	    }
+	 //   cout<<endl;
 	 }
+	 //   cout<<endl;
+      
       }
       
       for(int i=0;i<SSIZE;i++)
@@ -174,5 +204,27 @@ void initBigram(double(&bigram)[SSIZE][SSIZE],vector<int> (&textInt),vector<vect
 	    bigram[i][j] = bigram[i][j]/sum;
 	    }
 	 }
-      
+     
+}
+
+int calcSSIZE(char* filename)
+{
+  char buffer[20];
+  fstream file;
+  char split_char = ' ';
+  string tempC;
+  int tempNum;
+  int maxNum = 0;
+  
+  file.open(filename,ios::in);
+  while(file.getline(buffer,sizeof(buffer),'\n'))
+       {
+       istringstream str(buffer);
+       getline(str,tempC,split_char);
+       tempNum = c2n(tempC);
+       //cout<<tempNum;
+       if(tempNum>maxNum)
+          maxNum = tempNum;
+       }
+  return maxNum+1;
 }
